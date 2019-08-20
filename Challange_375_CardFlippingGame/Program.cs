f3f3576efd82eb7f18c186fd253052f2f0c77e32
Matt <Mattmsj24@gmail.com>
2019-08-19 21:46:25 -0700

    Add project files.

---
+++ Challange_375_CardFlippingGame/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Challange_375_CardFlippingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public string getCardLayout()
+        {
+            string cardLayout;
+
+            Console.WriteLine("please enter your cards");
+            cardLayout = Console.ReadLine();
+
+            return cardLayout;
+        }
+    }
+}
